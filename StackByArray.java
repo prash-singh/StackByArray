785dda567b3a05f430df98b0033595f36ab8a869
Prashant Singh <prashantsingh2k2@gmail.com>
2021-02-27 09:19:20 +0530

    StackByArray

---
+++ StackByArray.java
@@ -0,0 +1,47 @@
+package stack.array;
+
+public class StackByArray {
+    int arr[];
+    int top;
+
+    public void insertSize(int size){
+        arr = new int[size];
+        top = -1;
+    }
+
+    public StackByArray() {
+
+    }
+
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+
+    public boolean isStackFull(){
+        return (top== arr.length-1);
+    }
+
+    public void push(int data){
+        if(!isStackFull()){
+            arr[top+1] = data;
+            top++;
+        }else{
+            System.out.println("Stack is full");
+        }
+    }
+
+    public void pop(){
+        if(!isEmpty()){
+            System.out.println(arr[top]);
+            arr[top]=0;
+            top=top-1;
+        }else{
+            System.out.println("queue.queue.array is empty");
+        }
+    }
+    public void display(){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ StackByArrayMain.java
@@ -0,0 +1,27 @@
+package stack.array;
+import java.util.*;
+
+public class StackByArrayMain {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       StackByArray obj = new StackByArray();
+//        System.out.println("Enter size of an queue.queue.array");
+//        int size = sc.nextInt();
+
+        obj.insertSize(5);
+        obj.push(20);
+        obj.push(30);
+        obj.push(40);
+        obj.push(50);
+        obj.push(60);
+        obj.display();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+
+        obj.display();
+
+
+    }
+}
